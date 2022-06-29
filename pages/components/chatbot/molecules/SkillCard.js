e13e596984d4557613065560ea8ae5e6cb4dd412
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-06-29 18:40:00 +0530

    build fixes

--- pages/components/chatbot/molecules/SkillCard.js
+++ pages/components/chatbot/molecules/SkillCard.js
@@ -1,12 +1,4 @@
-import {
-  Box,
-  Button,
-  ButtonBase,
-  Card,
-  Grid,
-  LinearProgress,
-  Typography,
-} from '@mui/material';
+import { Box, Button, ButtonBase, Card, Grid, LinearProgress, Typography } from '@mui/material';
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { Avatar } from '@mui/material';
@@ -26,32 +18,29 @@ function SkillCard({ name, skills, isDetailed }) {
       width={isDetailed ? '500px' : '100%'}
     >
       <Typography variant="h6">{name}</Typography>
-      {skills.map(([name, val], idx) => {
-        return (
-          <Grid
-            container
-            display="flex"
-            flexDirection="row"
-            key={idx}
-            width="100%"
-            alignItems="center"
-            sx={{ paddingTop: '3px' }}
-          >
-            <Grid item xs={3} md={1}>
-              <Typography>{name}</Typography>
+      {skills &&
+        skills.map(([name, val], idx) => {
+          return (
+            <Grid
+              container
+              display="flex"
+              flexDirection="row"
+              key={idx}
+              width="100%"
+              alignItems="center"
+              sx={{ paddingTop: '3px' }}
+            >
+              <Grid item xs={3} md={1}>
+                <Typography>{name}</Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <Box width="50%" marginLeft={2}>
+                  <LinearProgress variant="determinate" value={val} sx={{ borderRadius: 2 }} />
+                </Box>
+              </Grid>
             </Grid>
-            <Grid item xs={8}>
-              <Box width="50%" marginLeft={2}>
-                <LinearProgress
-                  variant="determinate"
-                  value={val}
-                  sx={{ borderRadius: 2 }}
-                />
-              </Box>
-            </Grid>
-          </Grid>
-        );
-      })}
+          );
+        })}
     </Card>
   );
 }
